0eb953015446516f98f6eefe40bdde0e051fd1e4
EnderKnighterror <106501005+EnderKnighterror@users.noreply.github.com>
2023-11-24 18:29:45 +1030

    Fishing game completed

---
+++ README.md
@@ -0,0 +1,2 @@
+The only file replaced is the user_scores and user_credentials
+Fish_info and Fish_mapping are also not replaced delete them will break the game
--- src/Fishing.py
+++ src/Fishing.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import random
+from tkinter import ttk
 
 ASSETS_FOLDER = "Assets"
 
@@ -8,7 +9,6 @@ class ReadingCSV:
     @staticmethod
     def read_csv(file_name):
         # Construct the path to the file in the assets folder
-        # Navigate up one level from the src directory and then into the assets directory
         current_dir = os.path.dirname(__file__)
         assets_dir = os.path.join(current_dir, '..', 'assets', file_name)
 
@@ -20,6 +20,27 @@ class ReadingCSV:
                 data[key] = row
         return data
     
+
+    @staticmethod
+    def check_user_credentials_file(file_name):
+        # Construct the path to the file in the assets folder
+        current_dir = os.path.dirname(__file__)
+        assets_dir = os.path.join(current_dir, '..', 'assets', file_name)
+
+        # Check if file exists
+        if not os.path.exists(assets_dir):
+            return False, "File not found."
+
+        # Check the headers
+        with open(assets_dir, mode='r') as file:
+            csv_reader = csv.reader(file)
+            headers = next(csv_reader, None)
+            if headers != ['Username', 'Password']:
+                return False, "Incorrect headers."
+
+        return True, "File is valid."
+
+    @staticmethod
     def read_user_credentials(file_name):
         # Similar logic to read_csv, but specifically for user credentials
         current_dir = os.path.dirname(__file__)
@@ -31,11 +52,35 @@ class ReadingCSV:
             for row in csv_reader:
                 users[row['Username']] = row['Password']
         return users
+    
+    @staticmethod
+    def ensure_user_credentials_file(file_name):
+        current_dir = os.path.dirname(__file__)
+        assets_dir = os.path.join(current_dir, '..', 'assets')
+        file_path = os.path.join(assets_dir, file_name)
 
-# Read the CSV files and store the data
+        if not os.path.exists(assets_dir):
+            os.makedirs(assets_dir)
+
+        if not os.path.exists(file_path):
+            with open(file_path, mode='w', newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=['Username', 'Password'])
+                writer.writeheader()
+
+        return file_path
+
 fish_mapping = ReadingCSV.read_csv('fish_mapping.csv')
 fish_info = ReadingCSV.read_csv('fish_info.csv')
-user_credentials = ReadingCSV.read_user_credentials('user_credentials.csv')
+    
+# Check the user credentials file before reading any CSV files
+file_check, message = ReadingCSV.check_user_credentials_file('user_credentials.csv')
+if not file_check:
+    print(f"Error: {message}")
+    # Handle the error, e.g., exit the program or create a default file
+else:
+    # If the file is valid, proceed to read the CSV files
+    user_credentials = ReadingCSV.read_user_credentials('user_credentials.csv')
+    # Continue with the rest of your program
 
 class FishingGame:
     @staticmethod
@@ -113,7 +158,24 @@ class LoginWindow:
         self.window = tk.Toplevel(parent)
         self.authenticate_callback = authenticate_callback
         self.register_callback = register_callback
+        self.authenticated_username = None  
 
+        # Set the size of the window
+        window_width = 300
+        window_height = 200
+
+        # Get the screen dimension
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
+
+        # Find the center position
+        center_x = int(screen_width / 2 - window_width / 2)
+        center_y = int(screen_height / 2 - window_height / 2)
+
+        # Set the position of the window to the center of the screen
+        self.window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+        # Rest of your code to add widgets goes here
         self.username_label = tk.Label(self.window, text="Username:")
         self.username_label.pack()
 
@@ -136,6 +198,8 @@ class LoginWindow:
         self.exit_button.pack()
 
         self.window.protocol("WM_DELETE_WINDOW", self.disable_event)
+
+        
     
     def register(self):
         # Get the username and password from the entry fields
@@ -156,9 +220,13 @@ class LoginWindow:
         password = self.password_entry.get()
         if self.authenticate_callback(username, password):
             messagebox.showinfo("Login Info", "Authentication successful!")
+            self.authenticated_username = username  # Store the username
             self.window.destroy()  # Close the login window
         else:
             messagebox.showerror("Login Info", "Authentication failed.")
+
+    def get_authenticated_username(self):
+        return self.authenticated_username
     
     def exit_app(self):
         self.window.destroy()
@@ -171,6 +239,12 @@ def register(username, password):
     # Path to the user credentials file
     current_dir = os.path.dirname(__file__)
     credentials_file = os.path.join(current_dir, '..', 'assets', 'user_credentials.csv')
+
+    # Check if the file exists, create it with headers if it doesn't
+    if not os.path.exists(credentials_file):
+        with open(credentials_file, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['Username', 'Password'])
+            writer.writeheader()
 
     # Check if the user already exists
     with open(credentials_file, mode='r', newline='') as file:
@@ -192,12 +266,29 @@ def register(username, password):
     return True
 
 # Function to authenticate user (to be used in callback)
-def authenticate(username, password):
+def authenticate(username, password, user_credentials):
     return user_credentials.get(username) == password
+    
 
 class MainWindow:
-    def __init__(self, parent):
+    def __init__(self, parent, username):
+        self.username = username # Stores username
         self.window = tk.Tk() if parent is None else tk.Toplevel(parent)
+
+        # Set the size of the window
+        window_width = 800
+        window_height = 600
+
+        # Get the screen dimension
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
+
+        # Find the center position
+        center_x = int(screen_width / 2 - window_width / 2)
+        center_y = int(screen_height / 2 - window_height / 2)
+
+        # Set the position of the window to the center of the screen
+        self.window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
         self.score_label = tk.Label(self.window, text="Score: 0")
         self.score_label.pack()
@@ -211,17 +302,89 @@ class MainWindow:
         self.total_score = 0
         self.catches = []
 
+        self.kept_fish_treeview = ttk.Treeview(self.window, columns=('Remove',), show='headings')
+        self.kept_fish_treeview.pack()
+        self.kept_fish_treeview.heading('Remove', text='Remove')
+        self.kept_fish_treeview.column('Remove', width=60)
+
+        # Button to remove selected fish
+        self.remove_fish_button = tk.Button(self.window, text="Remove Selected Fish", command=self.remove_fish)
+        self.remove_fish_button.pack()
+
+        # Dictionary to store fish and their scores
+        self.kept_fish = {}
+
+        # Add an exit button to save score and exit
+        self.exit_button = tk.Button(self.window, text="Exit Game", command=self.exit_game)
+        self.exit_button.pack()
+
     def play_round(self):
         def gui_callback(message):
             return messagebox.askyesno("Keep Catch", message)
         
         catch, score = FishingGame.play_round(gui_callback)
-        self.total_score += score
-        if score > 0:
-            self.catches.append(catch)
+        self.catches.append(catch)
 
+        # Automatically keep 'Lost Bait' and 'Seaweed'
+        if catch in ["Lost bait", "Seaweed Monster (random clump of seaweed)"]:
+            keep_fish = True
+        else:
+            keep_fish = gui_callback(f"Do you want to keep the {catch}?")
+
+        if keep_fish:
+            self.total_score += score
+            # Ensure the fish name is correctly used as the key
+            self.kept_fish[catch] = score
+            self.kept_fish_treeview.insert('', 'end', values=(catch, '❌' if catch not in ["Lost bait", "Seaweed Monster (random clump of seaweed)"] else ''))
+        else:
+            # Update the score but do not add the fish to the kept list
+            self.total_score += FishingGame.calculate_score(catch, False)
+
+        self.update_score_and_catches()
+
+    def remove_fish(self):
+        selected_item = self.kept_fish_treeview.selection()
+        if selected_item:
+            fish = self.kept_fish_treeview.item(selected_item, 'values')[0]
+            # Prevent removal of 'Lost Bait' and 'Seaweed'
+            if fish in ["Lost bait", "Seaweed Monster (random clump of seaweed)"]:
+                messagebox.showinfo("Info", f"You cannot remove {fish}.")
+                return
+
+            # Check if the fish is in the kept_fish dictionary
+            if fish in self.kept_fish:
+                score_for_keeping = self.kept_fish[fish]
+                score_for_releasing = FishingGame.calculate_score(fish, False)
+                self.total_score -= score_for_keeping
+                self.total_score += score_for_releasing
+
+                del self.kept_fish[fish]
+                self.kept_fish_treeview.delete(selected_item)
+                self.update_score_and_catches()
+            else:
+                messagebox.showerror("Error", f"{fish} not found in kept fish.")
+
+
+    def update_score_and_catches(self):
         self.score_label.config(text=f"Score: {self.total_score}")
-        self.catches_text.insert(tk.END, f"{catch}\n")
+        self.catches_text.delete(1.0, tk.END)
+        self.catches_text.insert(tk.END, "\n".join(self.catches))
+
+    def exit_game(self):
+        self.save_score()
+        self.window.destroy()
+        root.destroy()  # Close the entire application
+
+    def save_score(self):
+        # Path to the scores file
+        scores_file = os.path.join(ASSETS_FOLDER, 'user_scores.csv')
+
+        # Append the score to the file
+        with open(scores_file, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([self.username, self.total_score])
+
+        messagebox.showinfo("Game Over", f"Your score of {self.total_score} was saved.")
 
     def start(self):
         self.window.mainloop()
@@ -230,11 +393,18 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.withdraw()  # Hide the root window
 
-    login_win = LoginWindow(root, authenticate, register)
+    # Ensure the user_credentials.csv file exists and read its content
+    user_credentials_file = ReadingCSV.ensure_user_credentials_file('user_credentials.csv')
+    user_credentials = ReadingCSV.read_user_credentials(user_credentials_file)
+
+    # Create and display the login window
+    login_win = LoginWindow(root, lambda u, p: authenticate(u, p, user_credentials), register)
 
     # Wait for the login window to close
     root.wait_window(login_win.window)
 
-    if not login_win.window.winfo_exists():  # If login was successful
-        main_win = MainWindow(root)
+    authenticated_username = login_win.get_authenticated_username()  # Retrieve the username
+    if authenticated_username:  # If login was successful
+        main_win = MainWindow(root, authenticated_username)
         main_win.start()
+
